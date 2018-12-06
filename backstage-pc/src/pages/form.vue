f0e060e913b989088087ac77401cbe9c81b70b3f
Baiguoshuai <1057459067@qq.com>
2018-12-07 01:08:23 +0800

    bgs给form页面添加了后台修改密码功能

--- backstage-pc/src/pages/form.vue
+++ backstage-pc/src/pages/form.vue
@@ -25,35 +25,48 @@
                 <div>
                   <a href="#" class="fresh1"><i class="glyphicon glyphicon-refresh"></i></a>
                   <a href="#" class="remove1"><i class="glyphicon glyphicon-remove"></i></a>
-
-
                 </div>
               </div>
               <div class="RecentBox-content">
-                <form class="form-horizontal">
+                <form class="form-horizontal" id="form1">
                   <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Normal input field</label>
                     <div class="col-sm-10">
-                      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                      <input type="email" class="form-control" id="inputEmail3" placeholder="ID" v-model="name">
                     </div>
                   </div>
                   <div class="form-group">
                     <label for="inputPassword3" class="col-sm-2 control-label">Password input field</label>
                     <div class="col-sm-10">
-                      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                      <input type="password" class="form-control" id="inputPassword3" placeholder="Password" @keyup="Nerr" @blur="loginCheck"v-model="psd">
+                      <span class="status status1"></span>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputPassword3" class="col-sm-2 control-label">New Password</label>
+                    <div class="col-sm-10">
+                      <input type="password" class="form-control" id="inputPassword4" v-model="newPsd" placeholder="Password" @keyup="Oninput" @blur="Oninput">
+                      <span class="status status2"></span>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputPassword3" class="col-sm-2 control-label">confirm Password</label>
+                    <div class="col-sm-10">
+                      <input type="password" class="form-control" id="inputPassword5" v-model="confirmPsd" placeholder="Password" @keyup="checkNewpsd" @blur="checkNewpsd">
+                      <span class="status status3"></span>
                     </div>
                   </div>
                   <label class="sr-only" for="inputHelpBlock">Input with help text</label>
                   <input type="text" id="inputHelpBlock" class="form-control" aria-describedby="helpBlock">
                   <span id="helpBlock" class="help-block">
                     A block of help text that breaks onto a new line and may extend beyond one line.</span>
-                  <textarea class="form-control" rows="3"></textarea>
+                  <textarea class="form-control" rows="3" placeholder="remarks"></textarea>
                 </form>
 
               </div>
               <div class="RecentBox-footer">
-                <input class="btn btn-default" type="submit" value="Submit">
-                <input class="btn btn-default" type="submit" value="Reset">
+                <input class="btn btn-default" type="submit" value="Submit" @click="updateMessage">
+                <input class="btn btn-default" type="submit" value="Reset" @click="resetMessage">
               </div>
             </div>
             <div class="QuickBox col-md-6">
@@ -310,13 +323,97 @@
   import TopComponents from "../components/TopComponents";
   import LeftComponents from "../components/LeftComponents";
     export default {
-        name: "form",
-      components: {LeftComponents, TopComponents}
+        name: "forms",
+      data(){
+          return{
+            name:'',
+            psd:'',
+            newPsd:'',
+            confirmPsd:'',
+            status:false
+          }
+      },
+      components: {LeftComponents, TopComponents},
+      methods:{
+          Nerr(){
+            $(".status").html("")
+          },
+          loginCheck(){
+            this.$http.post("http://bgs09143010.gotoip1.com/MeiYuan-webapp-php/loginCheck.php", {
+              "username":this.name,
+              "userpass":this.psd
+            }, {
+              emulateJSON: true
+            }).then((res) => {
+              if(res.data==this.name){
+                $(".status1").html("√")
+                $(".status1").css({color:"mediumspringgreen"})
+                this.status=true
+              }else{
+                $(".status1").html("×")
+                $(".status1").css({color:"red"})
+                this.status=false
+              }
+            })
+          },
+        Oninput(){
+            if(this.newPsd.length>6){
+              $(".status2").html("√")
+              $(".status2").css({color:"mediumspringgreen"})
+              this.status=true
+            }else{
+              $(".status2").html("×")
+              $(".status2").css({color:"red"})
+              this.status=false
+            }
+        },
+        checkNewpsd(){
+            if(this.newPsd==this.confirmPsd){
+              $(".status3").html("√")
+              $(".status3").css({color:"mediumspringgreen"})
+              this.status=true
+            }else{
+              $(".status3").html("×")
+              $(".status3").css({color:"red"})
+              this.status=false
+            }
+        },
+        updateMessage(){
+            if(this.status==true){
+              this.$http.post("http://bgs09143010.gotoip1.com/Backstage-php/updateUserpsd.php", {
+                "username":this.name,
+                "userpass":this.newPsd
+              }, {
+                emulateJSON: true
+              }).then((res) => {
+                if(res.data==1){
+                  alert("修改成功")
+                  $("#form1").find("input").val("")
+                  $("#form1").find(".status").html("")
+                }else{
+                  alert("修改失败")
+                }
+              })
+            }
+        },
+        resetMessage(){
+            $("#form1").find("input").val("")
+            $("#form1").find(".status").html("")
+        }
+      }
 
     }
 </script>
 
 <style scoped>
+  .status{
+    position: absolute;right: -1rem;top:.8rem;width: 1.8rem;height: 1.8rem;
+    text-align: center;display: flex;align-items: center;flex-direction: column;
+    font-size: 1.6rem;
+    -webkit-border-radius: 50%;
+    -moz-border-radius: 50%;
+    border-radius: 50%;
+  }
   body{
     background-image: url(../assets/img/bg1.jpg);
   }
